64d921372158e050039689d0dd08957aa2aec5d6
KalashBankey <143742175+KalashBankey@users.noreply.github.com>
2025-05-11 17:28:07 +0530

    Add files via upload

---
+++ qualifier/src/test/java/com/bfh/qualifier/BfhQualifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfh.qualifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfhQualifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
